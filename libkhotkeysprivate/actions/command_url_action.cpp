1d60f46f0fce96731b1df59f59def9ae5ec7a9c6
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-05-25 23:29:07 +0200

    let KRun do its job in peace

--- libkhotkeysprivate/actions/command_url_action.cpp
+++ libkhotkeysprivate/actions/command_url_action.cpp
@@ -28,8 +28,8 @@
 #include <KRun>
 #include <KService>
 #include <KUriFilter>
-#include <kworkspace.h>
 
+#include <QApplication>
 #include <QDebug>
 #include <QUrl>
 
@@ -86,69 +86,9 @@ const QString CommandUrlAction::description() const
 
 void CommandUrlAction::execute()
     {
-    qDebug();
     if( command_url().isEmpty())
         return;
-    KUriFilterData uri;
-    QString cmd = command_url();
-    static bool sm_ready = false;
-    if( !sm_ready )
-        {
-        KWorkSpace::propagateSessionManager();
-        sm_ready = true;
-        }
-//    int space_pos = command_url().find( ' ' );
-//    if( command_url()[ 0 ] != '\'' && command_url()[ 0 ] != '"' && space_pos > -1
-//        && command_url()[ space_pos - 1 ] != '\\' )
-//        cmd = command_url().left( space_pos ); // get first 'word'
-    uri.setData( cmd );
-    KUriFilter::self()->filterUri( uri );
-    if( uri.uri().isLocalFile() && !uri.uri().hasFragment() )
-        cmd = uri.uri().toLocalFile();
-    else
-        cmd = uri.uri().url();
-    switch( uri.uriType())
-        {
-        case KUriFilterData::LocalFile:
-        case KUriFilterData::LocalDir:
-        case KUriFilterData::NetProtocol:
-        case KUriFilterData::Help:
-            {
-            ( void ) new KRun( uri.uri(),0L);
-          break;
-            }
-        case KUriFilterData::Executable:
-            {
-            if (!KAuthorized::authorizeKAction("shell_access"))
-                return;
-            if( !uri.hasArgsAndOptions())
-                {
-                KService::Ptr service = KService::serviceByDesktopName( cmd );
-                if( service )
-                    {
-                    auto *job = new KIO::ApplicationLauncherJob(service);
-                    job->setUiDelegate(new KDialogJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, nullptr));
-                    job->start();
-                  break;
-                    }
-                }
-            // fall though
-            }
-        case KUriFilterData::Shell:
-            {
-            if (!KAuthorized::authorizeKAction("shell_access"))
-                return;
-            const QString exec = cmd + ( uri.hasArgsAndOptions() ? uri.argsAndOptions() : QString() );
-            auto *job = new KIO::CommandLauncherJob(exec);
-            job->setUiDelegate(new KDialogJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, nullptr));
-            job->setExecutable(cmd);
-            job->setIcon(uri.iconName());
-            job->start();
-          break;
-            }
-        default: // error
-          return;
-        }
+    new KRun(QUrl(command_url()), QApplication::activeWindow());
     }
 
 
