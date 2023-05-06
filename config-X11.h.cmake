3f87874556cfdf16cc5d2a5125fe0d3edf80e253
Nicolas Fella <nicolas.fella@gmx.de>
2023-05-06 16:16:32 +0200

    KHotkeys is dead
    This has been effectively the case for a long time
    
    For Plasma 6 we stop releasing it
    
    See https://phabricator.kde.org/T2050

--- config-X11.h.cmake
+++
@@ -1,5 +0,0 @@
-/* Define if you have the XTest extension */
-#cmakedefine HAVE_XTEST 1
-
-/* Define if you have X11 at all */
-#cmakedefine01 HAVE_X11
--- libkhotkeysprivate/triggers/triggers.cpp
+++
@@ -1,8 +0,0 @@
-/*
-   SPDX-FileCopyrightText: 1999-2001 Lubos Lunak <l.lunak@kde.org>
-   SPDX-FileCopyrightText: 2008 Michael Jansen <kde@michael-jansen.biz>
-
-   SPDX-License-Identifier: LGPL-2.0-only
-*/
-
-#include "moc_triggers.cpp"
